7059186013edfe84297eab2e665910946892d115
Nathan Printz <nprintz@Fry.local>
2015-03-02 14:28:14 -0600

    pypi/setuptools files
    added description, manifest and setup.py for package distribution.

---
+++ setup.py
@@ -0,0 +1,100 @@
+""" Packaging setup global function. """
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='jaide',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='2.0.0',
+
+    description='A tool for manipulating Junos devices.',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/NetworkAutomation/jaide',
+
+    # Author details
+    author='Nathan Printz <nkprintz@gmail.com> and Geoff Rhodes <geofflrhodes@gmail.com>',
+    author_email='nkprintz@gmail.com',
+
+    # Choose your license
+    license='GNU GPL',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Networking',
+        'Topic :: Utilities',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Natural Language :: English',
+    ],
+
+    # What does your project relate to?
+    keywords='networking junos juniper ',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['examples', 'testing']),
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'ncclient>=0.4.2',
+        'scp>=0.8.0,<1.0.0',
+        'paramiko>=1.14.0,<2.0.0',
+        'colorama>0.3.2'
+    ],
+
+    # List additional groups of dependencies here (e.g. development dependencies).
+    # You can install these using the following syntax, for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'jaide=jaide.jaide_cli:main',
+        ],
+    },
+)
