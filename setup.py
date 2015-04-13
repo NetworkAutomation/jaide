546a132ec756bc5c3f3b09985ffe59a6c3c59acf
Nathan Printz <nprintz@nprintz-mbpro.local>
2015-04-13 17:57:27 -0500

    move dev status to stable.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
